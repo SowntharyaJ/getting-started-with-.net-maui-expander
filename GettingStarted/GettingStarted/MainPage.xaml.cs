754fcd128f689444adf647a285dd947e67c75723
SowntharyaJ <sowntharya.jayamoorthy@syncfusion.com>
2026-07-13 16:13:30 +0530

    Resolved warnings and updated the sample to .NET 10

---
+++ GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
